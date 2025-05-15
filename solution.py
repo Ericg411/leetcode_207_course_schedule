458448d0f6d04e8bd476ea135de93044c687bb97
Ericg411 <ericg411@gmail.com>
2025-05-14 21:39:25 -0500

    updated algorithm one last time to solidify my understanding

--- solution.py
+++ solution.py
@@ -3,21 +3,23 @@ from collections import deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
         in_degree = [0] * numCourses
-        adj = [[] for _ in range(numCourses)]
+        adjacency = [[] for _ in range(numCourses)]
 
         for prereq in prerequisites:
-            adj[prereq[1]].append(prereq[0])
             in_degree[prereq[0]] += 1
+            adjacency[prereq[1]].append(prereq[0])
 
         queue = deque([i for i in range(numCourses) if in_degree[i] == 0])
-        processed_courses = 0
+        courses_taken = 0
 
         while queue:
-            course = queue.popleft()
-            processed_courses += 1
-            for next_course in adj[course]:
-                in_degree[next_course] -= 1
-                if in_degree[next_course] == 0:
-                    queue.append(next_course)
+            current = queue.popleft()
+            courses_taken += 1
+            print(current)
+            next_courses = adjacency[current]
+            for course in next_courses:
+                in_degree[course] -= 1
+                if in_degree[course] == 0:
+                    queue.append(course)
 
-        return processed_courses == numCourses
+        return courses_taken == numCourses
